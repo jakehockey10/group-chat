c6068595a941c352f55d43c037f41398efb5b7d7
Jake Smith <jakehockey10@gmail.com>
2016-09-30 02:11:01 -0600

    Go Rails Group Chat Tutorial

---
+++ app/jobs/message_relay_job.rb
@@ -0,0 +1,13 @@
+class MessageRelayJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}",
+                                 {
+                                     message:     MessagesController.render(message),
+                                     username: message.user.username,
+                                     body: message.body,
+                                     chatroom_id: message.chatroom.id
+                                 }
+  end
+end
