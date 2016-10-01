1abb4e1321e8feff665b089660905d2d8b344f19
Jake Smith <jakehockey10@gmail.com>
2016-09-30 20:45:20 -0600

    move to redis cloud

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://limitless-fjord-25965.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://limitless-fjord-25965.herokuapp.com', /http:\/\/limitless-fjord-25965.herouapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://limitless-fjord-25965.herokuapp.com', 'http://limitless-fjord-25965.herokuapp.com/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
