694206f753587e31ccaa7561699ec6b7d90140a0
Jake Smith <jakehockey10@gmail.com>
2016-10-05 00:29:33 -0600

    ignore that last commit; more recent documentation says this is it

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -4,12 +4,14 @@ if Rails.env.production?
       cache: Shrine::Storage::S3.new(
           access_key_id:     ENV['S3_ACCESS_KEY_ID'],
           secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+          region:            'us-east-1',
           bucket:            'group-chat-jakehockey10',
           prefix:            'cache'
       ),
       store: Shrine::Storage::S3.new(
           access_key_id:     ENV['S3_ACCESS_KEY_ID'],
           secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+          region:            'us-east-1',
           bucket:            'group-chat-jakehockey10',
           prefix:            'store'
       )
