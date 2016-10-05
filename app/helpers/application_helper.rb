889d17f7656fab8f832c2371cc7d1aa4a7865416
Jake Smith <jakehockey10@gmail.com>
2016-10-05 00:15:08 -0600

    gravatar and fix secrets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # http://railscasts.com/episodes/244-gravatar?view=asciicast
+  def avatar_url(object)
+    if object.image_url.present?
+      object.image_url
+    else
+      gravatar_id = Digest::MD5::hexdigest(object.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png"
+    end
+  end
+
 end
